19d19ff020ee51f0e60727135bf7d577daf78717
Eric Koslow <ekoslow@gmail.com>
2014-10-03 12:46:44 -0700

    Use cool looking product images. :tshirt:

--- lib/springtee/product.rb
+++ lib/springtee/product.rb
@@ -7,8 +7,8 @@ module Springtee
   end
 
   Product::PRODUCTS = {
-    1 => Product.new(1, "Mac's Brown Bottle Ale", 'http://images.teespring.com/shirt_pic/1320515/765931/2/566/front.jpg?v=2014-09-14-13-49'),
-    2 => Product.new(2, 'Tees for the Tesla Museum!', 'http://images.teespring.com/shirt_pic/1177616/550027/6/647/front.jpg?v=2014-08-07-21-11')
+    1 => Product.new(1, "Mac's Brown Bottle Ale", 'http://images.teespring.com/shirt_pic/1320515/765931/2/566/front.jpg?v=2014-09-14-13-49&product-image-id=22'),
+    2 => Product.new(2, 'Tees for the Tesla Museum!', 'http://images.teespring.com/shirt_pic/1177616/550027/6/647/front.jpg?v=2014-08-07-21-11&product-image-id=22')
   }
 end
 
